e55bd44813b6691f6b10ed9b019e0ca5623ca6d7
Zach Maas <zach.maas@gmail.com>
2017-10-08 21:57:29 -0600

    Add a hydra for smerge-mode, if ediff isn't needed.

--- core/tools/hydras.el
+++ core/tools/hydras.el
@@ -23,6 +23,33 @@ _k_prev	_d_iff
 ("d" magit-diff-buffer-file "diff")
 ("q" nil "quit"))
 
+(defhydra ono-smerge
+  (:color red :hint nil
+          :pre (smerge-mode 1))
+  "
+^Move^ ^Keep^ ^Diff^ ^Pair^
+------------------------------------------------------
+_n_ext _b_ase _R_efine _<_: base-mine
+_p_rev _m_ine _E_diff _=_: mine-other
+^ ^ _o_ther _C_ombine _>_: base-other
+^ ^ _a_ll _r_esolve
+_q_uit _RET_: current
+"
+  ("RET" smerge-keep-current)
+  ("C" smerge-combine-with-next)
+  ("E" smerge-ediff)
+  ("R" smerge-refine)
+  ("a" smerge-keep-all)
+  ("b" smerge-keep-base)
+  ("m" smerge-keep-mine)
+  ("n" smerge-next)
+  ("o" smerge-keep-other)
+  ("p" smerge-prev)
+  ("r" smerge-resolve)
+  ("<" smerge-diff-base-mine)
+  ("=" smerge-diff-mine-other)
+  (">" smerge-diff-base-other)
+	("q" nil :color blue))
 
 ;; A hydra for org hot expansion of templates.
 (defhydra hydra-org-template (:color blue :hint nil)

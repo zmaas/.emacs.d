b6b23740c93800700c8f8824dc262a1a853b71a7
Zach Maas <zach.maas@gmail.com>
2017-10-23 11:41:19 -0600

    More cleanup for archives.el, fixed update command.

--- core/startup/archives.el
+++ core/startup/archives.el
@@ -7,9 +7,9 @@
 
 ;; Configure package archives
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+												 ("org" . "https://orgmode.org/elpa/")
+												 ("marmalade" . "https://marmalade-repo.org/packages/")
+												 ("melpa" . "https://melpa.org/packages/")))
 
 ;; Force GNUtls to use real security
 (setq gnutls-min-prime-bits 4096)
@@ -29,9 +29,9 @@
 ;;; Make sure we have use-package
 (quelpa
  '(use-package
-    :fetcher github
-    :repo "jwiegley/use-package"
-    :files ("use-package.el")))
+		:fetcher github
+		:repo "jwiegley/use-package"
+		:files ("use-package.el")))
 (quelpa
  '(quelpa-use-package
    :fetcher github
@@ -41,30 +41,30 @@
 
 ;; Load auto-compile for any future editing of elisp files
 (use-package auto-compile
-  :ensure t
-  :config
-  (add-hook 'after-init-hook #'auto-compile-mode)
-  (auto-compile-on-save-mode t)
-  (auto-compile-on-load-mode t))
+	:ensure t
+	:config
+	(add-hook 'after-init-hook #'auto-compile-mode)
+	(auto-compile-on-save-mode t)
+	(auto-compile-on-load-mode t))
 
 ;; Always prefer newer files when loading
 (setq load-prefer-newer t)
 
 ;; Paradox for better package viewing
 (use-package paradox
-  :ensure t
-  :config
-  (paradox-enable))
+	:ensure t
+	:config
+	(paradox-enable))
 
-(defun ono-update-packages
-    "Updates all of our packages in ELPA and from QUELPA"
-  (interactive)
-  (package-refresh-contents)
-  (paradox-upgrade-packages)
-  (quelpa-self-upgrade)
-  (quelpa-upgrade))
+(defun ono-update-packages ()
+	"Updates all of our packages in ELPA and from QUELPA"
+	(interactive)
+	(package-refresh-contents)
+	(paradox-upgrade-packages)
+	(quelpa-self-upgrade)
+	(quelpa-upgrade))
 
 (defun ono-re-init ()
-  "Reloads init.el after any changes that we have made."
-  (interactive)
-  (load user-init-file))
+	"Reloads init.el after any changes that we have made."
+	(interactive)
+	(load user-init-file))

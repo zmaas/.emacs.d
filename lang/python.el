7528d1141ffc2ec079648558c0a6045eefb857fb
Zach Maas <zach.maas@gmail.com>
2017-11-13 19:51:44 -0700

    Changes to python for documentation support
    - sphinx-doc provides a simple function to add docstrings to python
      code
    - add proper leader keybindings to python and derived maps

--- lang/python.el
+++ lang/python.el
@@ -9,9 +9,11 @@
 ;; IN PROGRESS - anaconda is good so far, some lag that needs to be figured out
 (use-package anaconda-mode
 	:ensure t
+	:diminish	anaconda-mode
 	:init
 	(use-package virtualenvwrapper
 		:ensure t
+		:diminish	""
 		:config
 		(venv-initialize-interactive-shells)
 		(venv-initialize-eshell))
@@ -43,8 +45,32 @@
 							(set (make-local-variable 'company-backends)											
 									 '((company-anaconda company-dabbrev-code company-yasnippet))))))
 
+;; Sphinx-doc -- Automagically add docstrings to python	functions
+(use-package sphinx-doc
+	:ensure t
+	:diminish	""
+	:config
+	(add-hook 'python-mode-hook (lambda  () (sphinx-doc-mode t))))
+
 ;; EIN - Emacs Ipython Notebooks
 (use-package ein
 	:ensure t)
+
+;; Mode	keybinds
+(general-define-key
+ :states '(normal visual insert emacs)
+ :keymaps '(python-mode-map inferior-python-mode-map)
+ :prefix "SPC"
+ :non-normal-prefix "M-SPC"
+ "l" '(:ignore t :which-key "layer")
+ "lr" '(run-python :which-key "python repl")
+ "lf" '(py-autopep8 :which-key "autoformat")
+ "le" '(python-shell-send-buffer :which-key "eval buffer")
+ "lE" '(python-shell-send-defun :which-key "eval defun")
+ "ld" '(anaconda-mode-find-definitions :which-key "def")
+ "lD" '(sphinx-doc :which-key "add docs")
+ "l?" '(anaconda-mode-show-doc :which-key "docs?")
+ "lv" '(venv-workon :which-key "venv-on")
+ "lV" '(venv-deactivate :which-key "venv-off"))
 
 ;;; zm-python-tools.el ends here

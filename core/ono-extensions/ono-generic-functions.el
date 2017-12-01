9218153a6f384628ed67465cb7501c54b3808832
Zach Maas <zach.maas@gmail.com>
2017-12-01 13:43:11 -0700

    Fix: Odd formatting in ono-generic-functions.el

--- core/ono-extensions/ono-generic-functions.el
+++ core/ono-extensions/ono-generic-functions.el
@@ -11,43 +11,43 @@
   (find-file "~/Dropbox/Org/organizer.org"))
 
 (defun ono-fill-or-unfill ()
-	"Like `fill-paragraph', but unfill if used twice."
-	(interactive)
-	(unfill-toggle))
+  "Like `fill-paragraph', but unfill if used twice."
+  (interactive)
+  (unfill-toggle))
 
 (defun lod ()
-	"Well. This is disappointing."
-	(interactive)
-	(insert "\u0ca0_\u0ca0"))
+  "Well. This is disappointing."
+  (interactive)
+  (insert "\u0ca0_\u0ca0"))
 
 (defcustom smart-to-ascii '(("\x201C" . "\"")
-														("\x201D" . "\"")
-														("\x2018" . "'")
-														("\x2019" . "'")
-														;; en-dash
-														("\x2013" . "-")
-														;; em-dash
-														("\x2014" . "-"))
-	"Map of smart quotes to their replacements"
-	:type '(repeat (cons (string :tag "Smart Character  ")
-											 (string :tag "Ascii Replacement"))))
+                            ("\x201D" . "\"")
+                            ("\x2018" . "'")
+                            ("\x2019" . "'")
+                            ;; en-dash
+                            ("\x2013" . "-")
+                            ;; em-dash
+                            ("\x2014" . "-"))
+  "Map of smart quotes to their replacements"
+  :type '(repeat (cons (string :tag "Smart Character  ")
+                       (string :tag "Ascii Replacement"))))
 
 (defun ono-smart-to-ascii (beg end)
-	"Replace smart quotes and dashes with their ASCII equivalents"
-	(interactive "r")
-	(format-replace-strings smart-to-ascii
-													nil beg end))
+  "Replace smart quotes and dashes with their ASCII equivalents"
+  (interactive "r")
+  (format-replace-strings smart-to-ascii
+                          nil beg end))
 
 (defun untabify-buffer ()
-	(interactive)
-	(untabify (point-min) (point-max)))
+  (interactive)
+  (untabify (point-min) (point-max)))
 
 (defun indent-buffer ()
-	(interactive)
-	(indent-region (point-min) (point-max)))
+  (interactive)
+  (indent-region (point-min) (point-max)))
 
 (defun ono-cleanup-buffer ()
-	"Perform a bunch of operations on the whitespace content of a
+  "Perform a bunch of operations on the whitespace content of a
 buffer." (interactive)
 (ono-smart-to-ascii (point-min) (point-max))
 (indent-buffer)
@@ -60,12 +60,12 @@ buffer." (interactive)
   (let ((closing nil))
     (save-excursion
       (while (condition-case nil
-								 (progn
-									 (backward-up-list)
-									 (let ((syntax (syntax-after (point))))
-										 (case (car syntax)
-											 ((4) (setq closing (cons (cdr syntax) closing)))
-											 ((7 8) (setq closing (cons (char-after (point)) closing)))))
-									 t)
-							 ((scan-error) nil))))
+                 (progn
+                   (backward-up-list)
+                   (let ((syntax (syntax-after (point))))
+                     (case (car syntax)
+                       ((4) (setq closing (cons (cdr syntax) closing)))
+                       ((7 8) (setq closing (cons (char-after (point)) closing)))))
+                   t)
+               ((scan-error) nil))))
     (apply #'insert (nreverse closing))))

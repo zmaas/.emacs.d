ef51943c9b6ad85f048ca05791124afe5061699f
Zach Maas <zach.maas@gmail.com>
2018-05-15 11:29:25 -0600

    Add ox-hugo	for	my blogging needs

---
+++ apps/hugo.el
@@ -0,0 +1,9 @@
+;;; hugo.el ---Setup for ox-hugo                      -*- lexical-binding: t; -*-
+
+;; Author: 	 <zach.maas@gmail.com>
+
+(use-package ox-hugo
+	:ensure t
+	:after ox)
+
+;; hugo.el ends here

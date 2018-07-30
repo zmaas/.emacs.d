911aebc9be544d98a82cc79cb5768a868d1d5fc3
Zach Maas <zach.maas@gmail.com>
2018-07-30 12:09:41 -0600

    Switch to dracula theme as default.

--- core/appearance/themes.el
+++ core/appearance/themes.el
@@ -26,12 +26,12 @@
 
 (use-package tao-theme
 	:ensure t)
-(use-package cyberpunk-theme
+;; (use-package cyberpunk-theme
+;; 	:ensure t)
+(use-package kaolin-themes
 	:ensure t)
-;; (use-package dracula-theme
-;; 	:ensure t)
-;; (use-package darktooth-theme
-;; 	:ensure t)
+(use-package dracula-theme
+	:ensure t)
 ;; (use-package challenger-deep-theme
 ;; 	:ensure t)
 ;; (use-package color-theme-sanityinc-solarized
@@ -47,81 +47,31 @@
 
 ;; Custom theme definitions. (spaceline-compile) is needed to get proper
 ;; powerline separators on theme-switch
+
 (defun ono-day-theme ()
-	"Set material-light theme for daytime and associated colors."
-	(interactive)
-	(!ono-set-theme 'material-light)
-	;; (set-face-background 'nlinum-relative-current-face "#EEEEEE")
-	;; (set-face-foreground 'nlinum-relative-current-face "#607D8B")
-	)
-
-
-(defun ono-day-alt-theme ()
 	"Set underwater theme for daytime and associated colors."
 	(interactive)
-	(!ono-set-theme 'challenger-deep)
-	;; (set-face-background 'nlinum-relative-current-face "#EEEEEE")
-	;; (set-face-foreground 'nlinum-relative-current-face "#607D8B")
-	)
-
-(defun ono-solarized-theme ()
-	"Set leuven theme for daytime and associated colors."
-	(interactive)
-	(!ono-set-theme 'sanityinc-solarized-light)
-	;; (set-face-background 'nlinum-relative-current-face "#EEEEEE")
-	;; (set-face-foreground 'nlinum-relative-current-face "#607D8B")
-	)
+	(!ono-set-theme 'kaolin-light))
 
 (defun ono-plain-night-theme ()
   "Set tao-yin theme for dark nightime usage."
   (interactive)
-  (!ono-set-theme 'tao-yin)
-	;; (set-face-background 'nlinum-relative-current-face "#000000")
-	)
+  (!ono-set-theme 'tao-yin))
 
 (defun ono-day-bright-theme ()
 	"Set eziam-light theme for daytime and associated colors."
 	(interactive)
-	(!ono-set-theme 'tao-yang)
-	;; (set-face-background 'nlinum-relative-current-face "#EEEEEE")
-	;; (set-face-foreground 'nlinum-relative-current-face "#607D8B")
-	)
+	(!ono-set-theme 'tao-yang))
 
-(defun ono-night-theme ()
-	"Set dracula theme for nighttime and associated colors."
-	(interactive)
-	;; (!ono-set-theme 'dracula)
-	;; (set-face-background 'nlinum-relative-current-face "#000000")
-	)
-
-(defun ono-night-alt-theme ()
-	"Set darktooth theme for nighttime and associated colors."
-	(interactive)
-	(!ono-set-theme 'darktooth)
-	;; (set-face-background 'nlinum-relative-current-face "#000000")
-	)
-
-(defun ono-tron-theme ()
-	"Set tron theme for nighttime and associated colors."
-	(interactive)
-	(!ono-set-theme 'zerodark)
-	;; (set-face-background 'nlinum-relative-current-face "#000000")
-	;; (set-face-background 'mode-line-emphasis)
-	)
-
-(defun ono-leuven-theme ()
-	"Set tron theme for nighttime and associated colors."
-	(interactive)
-	(!ono-set-theme 'leuven)
-	;; (set-face-background 'nlinum-relative-current-face "#000000")
-	;; (set-face-background 'mode-line-emphasis)
-	)
+;; (defun ono-night-theme ()
+;; 	"Set dracula theme for nighttime and associated colors."
+;; 	(interactive)
+;; 	(!ono-set-theme 'dracula)
+;; 	)
 
 (defun ono-dark-night-theme ()
   "Set cyberpunk theme for dark nightime usage."
   (interactive)
-  (!ono-set-theme 'cyberpunk)
-	;; (set-face-background 'nlinum-relative-current-face "#000000")
-	)
+  (!ono-set-theme 'dracula))
 
 ;; themes.el ends here

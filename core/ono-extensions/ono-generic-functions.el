bcfc1b913457dfae50cea956a704a0602271e11a
Zach Maas <zach.maas@gmail.com>
2018-07-30 12:11:50 -0600

    Have successful compilation buffers automatically buried.

--- core/ono-extensions/ono-generic-functions.el
+++ core/ono-extensions/ono-generic-functions.el
@@ -94,3 +94,22 @@ buffer." (interactive)
                    t)
                ((scan-error) nil))))
     (apply #'insert (nreverse closing))))
+
+(defun bury-compile-buffer-if-successful (buffer string)
+	"Bury a compilation buffer if succeeded without warnings "
+	(when (and
+         (buffer-live-p buffer)
+         (string-match "compilation" (buffer-name buffer))
+         (string-match "finished" string)
+         (not
+          (with-current-buffer buffer
+            (goto-char (point-min))
+            (search-forward "warning" nil t))))
+    (run-with-timer 2 nil
+                    (lambda (buf)
+                      (bury-buffer buf)
+                      (switch-to-prev-buffer (get-buffer-window buf) 'kill))
+                    buffer)))
+(add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
+
+;;;	ono-generic-functions.el ends here
